7828116f66eb29362bc4d21c0d74260b4870902e
CLQuantizer <67228230+CLQuantizer@users.noreply.github.com>
2023-01-05 16:47:03 +0000

    Create gpio_led.py
    init count from 0 to 127

---
+++ gpio_led.py
@@ -0,0 +1,18 @@
+from gpiozero import LED
+from time import sleep
+
+# declare the ligts
+l = []
+for num in [16, 21, 20, 12, 24, 23, 18]:
+    l.append(LED(num))
+
+def display(i: int, lights):
+    for (j,led) in enumerate(lights):
+        if 2**j&i==2**j:
+            led.on()
+        else:
+            led.off()
+while True:
+    for i in range(124):
+        display(i, l)
+        sleep(0.3)
